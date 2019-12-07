bb179b8fdb1d9f314d19bfdeca07fd6411ca15ae
Skully <pawnage321@hotmail.com>
2019-12-07 22:02:38 +0000

    Parallel & support for visualization.

---
+++ output/api.php
@@ -0,0 +1,4 @@
+<?php
+define('WORLD_HEIGHT', 30);
+define('WORLD_WIDTH', 30);
+define('GENERATIONS', 30);
--- populateWorld.c
+++ populateWorld.c
@@ -1,14 +1,39 @@
 #include "header.h"
 
+// Child function for multi-threaded generation.
+void *thread_populateWorld(void* thread_id)
+{
+	long threadID = (long) thread_id;
+	int x, y; // X and Y index of the world array.
+	int rowsPerThread = ((WORLD_HEIGHT * WORLD_WIDTH) / TOTAL_THREADS) * threadID; // Get the total number of rows each thread needs to handle.
+
+	for (x = rowsPerThread; x < WORLD_WIDTH; x++)
+	{
+		for (y = rowsPerThread; y < WORLD_HEIGHT; y++)
+		{
+			WORLD_ARRAY[x][y] = getRandomState();
+		}
+	}
+}
+
 // Takes the given 2D array and populates each index with a random cell state.
 void populateWorld()
 {
-	int i, j;
-	for (i = 0; i < WORLD_WIDTH; i++)
+	long thread;
+	pthread_t *allThreads; // Define pointer to contain all threads.
+
+	allThreads = malloc(TOTAL_THREADS * sizeof(pthread_t)); // Assign threads to pointer.
+
+	// Assign eaceh thread to function.
+	for (thread = 0; thread < TOTAL_THREADS; thread++)
 	{
-		for (j = 0; j < WORLD_HEIGHT; j++)
-		{
-			WORLD_ARRAY[i][j] = getRandomState();
-		}
+		pthread_create(&allThreads[thread], NULL, thread_populateWorld, (void *) thread);
 	}
+
+	for (thread = 0; thread < TOTAL_THREADS; thread++)
+	{
+		pthread_join(allThreads[thread], NULL);
+	}
+
+	free(allThreads); // Free thread allocations.
 }
