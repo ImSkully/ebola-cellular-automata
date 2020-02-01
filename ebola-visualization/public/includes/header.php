57e97f92c9bb601ab51c9456e29d0543cdf807ab
ImSkully <faran.bari@hotmail.com>
2020-02-01 14:57:40 +0000

    Initial commit

---
+++ ebola-visualization/public/includes/header.php
@@ -0,0 +1,15 @@
+
+<meta charset="utf-8">
+<meta name="description" content="Cellular Automata Visualization">
+<meta name="keywords" content="cellular, automata, visualization">
+<meta property="og:type" content="website">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+
+<!-- CSS (Bootstrap) & FontAwesome -->
+<link rel="stylesheet" href="../css/style.css" />
+<link href="css/fontawesome/css/all.min.css" rel="stylesheet" type="text/css">
+
+<!-- Javascript -->
+<script src="https://code.jquery.com/jquery-1.11.1.min.js"></script> <!-- JQuery -->
+<script src="js/bootstrap.bundle.js"></script> <!-- Bootstrap -->
